4b9e0f78bd79a028c600c7e09230d73bf36f8411
Anicet Ngrt <anicet.nougaret@zohomail.eu>
2021-01-30 13:09:56 +0100

    removed useless deps

--- umbrella_server/apps/server/mix.exs
+++ umbrella_server/apps/server/mix.exs
@@ -22,7 +22,6 @@ defmodule Server.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:esockd, "~>5.5.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
