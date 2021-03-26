82800595ae31babd80ee789e4c420d9761e8fff6
Anicet <an.nougaret@gmail.com>
2021-03-26 10:46:34 +0100

    trying something

--- homesynck/lib/homesynck_web/endpoint.ex
+++ homesynck/lib/homesynck_web/endpoint.ex
@@ -7,17 +7,17 @@ defmodule HomesynckWeb.Endpoint do
   It receives the endpoint configuration and checks if
   configuration should be loaded from the system environment.
   """
-  def init(_key, config) do
-    if config[:load_from_system_env] do
-      port =
-        Application.get_env(:homesynck, :app_port) ||
-          raise "expected the PORT environment variable to be set"
+  # def init(_key, config) do
+  #   if config[:load_from_system_env] do
+  #     port =
+  #       Application.get_env(:homesynck, :app_port) ||
+  #         raise "expected the PORT environment variable to be set"
 
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
-    else
-      {:ok, config}
-    end
-  end
+  #     {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+  #   else
+  #     {:ok, config}
+  #   end
+  # end
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
