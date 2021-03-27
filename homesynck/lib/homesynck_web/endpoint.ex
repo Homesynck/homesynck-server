4893bfbac47ed3e2997864be9adca4423eaf5547
Anicet <an.nougaret@gmail.com>
2021-03-27 14:38:32 +0100

    try fix image

--- homesynck/lib/homesynck_web/endpoint.ex
+++ homesynck/lib/homesynck_web/endpoint.ex
@@ -23,7 +23,7 @@ defmodule HomesynckWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :homesynck,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
