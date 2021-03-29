d1b47796b8b47d3be8d2925d158d70ffe56d6c45
AnicetNgrt <anicet.nougaret@zohomail.eu>
2021-03-29 17:03:30 +0200

    improved comments

--- homesynck/config/prod.secret.exs
+++ homesynck/config/prod.secret.exs
@@ -8,10 +8,30 @@ use Mix.Config
 #     """
 
 config :homesynck,
+  # Admin account is an account created on server start
+  # Usefull if you plan to disable register and allow only 1 account
   enable_admin_account: true,
   admin_username: System.get_env("ADMIN_USERNAME"),
   admin_password: System.get_env("ADMIN_PASSWORD"),
   enable_register: true,
+  # Phone validation can be disabled. If enabled you will need your own
+  # phone validation API.
+  # Compliant phone validation APIs must:
+  #  - Accept POST on / with Json body:
+  #     {
+  #       "number": <user phone number>,
+  #       "message": <generated register token>,
+  #       "secret": <api key if required>
+  #     }
+  #  - Reply with code 200 if success and body = "0"
+  #
+  # Phone validation is a spam prevention feature that can be useful for
+  # big audience apps.
+  # If activated a register token will be sent to a phone number validation API.
+  # This API is then meant to send the token to the phone owner through automated SMS.
+  # Then, the user would use this token to register one account.
+  # Phone numbers used to generate a register token will be on a cooldown and
+  # won't be authorized to generate another register token for 30 days.
   enable_phone_validation: true,
   phone_validation_api_endpoint: System.get_env("PHONE_VALIDATION_API_ENDPOINT") || "",
   phone_validation_api_key: System.get_env("PHONE_VALIDATION_API_KEY") || ""
